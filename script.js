8076f235140e9406f22b41d179560378e4572357
Jerry Miao <jm2284@cornell.edu>
2019-05-02 04:33:58 -0400

    scroll click

--- script.js
+++ script.js
@@ -3,29 +3,39 @@ $(document).ready(() => {
     $(event.currentTarget).addClass("highlight");
   }).on('mouseleave', event => {
     $(event.currentTarget).removeClass("highlight");
+  }).on('click', () => {
+    $('#first-section')[0].scrollIntoView(true);
   });
 
   $('#nav-subteams').on('mouseenter', event => {
     $(event.currentTarget).addClass("highlight");
   }).on('mouseleave', event => {
     $(event.currentTarget).removeClass("highlight");
+  }).on('click', () => {
+    $('#second-section')[0].scrollIntoView(true);
   });
 
   $('#nav-members').on('mouseenter', event => {
     $(event.currentTarget).addClass("highlight");
   }).on('mouseleave', event => {
     $(event.currentTarget).removeClass("highlight");
+  }).on('click', () => {
+    $('#third-section')[0].scrollIntoView(true);
   });
 
   $('#nav-recruiting').on('mouseenter', event => {
     $(event.currentTarget).addClass("highlight");
   }).on('mouseleave', event => {
     $(event.currentTarget).removeClass("highlight");
+  }).on('click', () => {
+    $('#fourth-section')[0].scrollIntoView(true);
   });
 
   $('#nav-sponsors').on('mouseenter', event => {
     $(event.currentTarget).addClass("highlight");
   }).on('mouseleave', event => {
     $(event.currentTarget).removeClass("highlight");
+  }).on('click', () => {
+    $('#fifth-section')[0].scrollIntoView(true);
   });
 });
